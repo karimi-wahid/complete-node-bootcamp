0e1e0e9513e0f4244e5b5a9a9c49b2f375512beb
Wahidullah Karimi <wahidullah.karimi22@gmail.com>
2026-02-15 09:14:21 +0430

    fs module

---
+++ 1-node-farm/starter/index.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+
+// Blocking, synchronous way
+// const textInp = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textInp);
+// const textOut = `This is what we know about the avocado: ${textInp}. \nCreated on ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOut);
+// console.log("File written successfully");
+
+// Non-blocking, asynchronous way
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+  fs.readFile(`./txt/${data1}`, "utf-8", (err, data2) => {
+    console.log(data2);
+    fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+      console.log(data3);
+
+      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
+        console.log("File written successfully");
+      });
+    });
+  });
+});
+console.log("Will read file...");
